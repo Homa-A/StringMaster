4323aad90f0ca8b789caf25668b68b6e81cba8fd
Amir.Homa <amirhomaunii@gmail.com>
2024-03-02 22:59:11 +0330

    add evon function

--- Base Structure/src/main/java/Advanced.java
+++ Base Structure/src/main/java/Advanced.java
@@ -9,8 +9,51 @@ public class Advanced {
      * You have to search the sentence to find the word that you were given as input and change it with the newWord
 
      */
-    public String wordCensor(String sentence, String word, String newWord){
-        return null;
+    public String wordCensor(String sentence, String word, String newWord)
+        {
+            char[] newsentence = new char[100];
+            char[] sentence1 = sentence.toCharArray();
+            char[] word1 = word.toCharArray();
+            char[] newword1= newWord.toCharArray();
+            boolean flag = true;
+            for (int i = 0 ; i < sentence.length() ; i++)
+            {
+                if (sentence1[i] == word1 [0])
+                {
+                    int j;
+                    for (j = 1 ; j < word.length() ; j++)
+                    {
+                        i ++;
+                        if (sentence1[i] != word1[j] )
+                        {
+                            flag = false;
+                        }
+                    }
+                    i = i - j;
+                        if (flag == true)
+                        {
+                            for (int k = 0; k < i; k++)
+                            {
+                                newsentence[k] = sentence1[k];
+                            }
+                            int z=0;
+                            for (int k = (i+1); k < newWord.length(); k++)
+                            {
+                                newsentence[k] = newword1[z];
+                                z++;
+                            }
+                            z=i + newWord.length();
+                            for (int k = i + word.length() ; k < sentence.length(); k++)
+                            {
+                                newsentence[z] = sentence1[k];
+                                z++;
+                            }
+                        }
+                }
+            }
+            String newsen = new String(newsentence);
+        return newsen;
+
     }
 
     /**
@@ -19,8 +62,39 @@ public class Advanced {
      * @param lastName is a last name with irregular letters (example : pOtTeR)
      * @return fullName is a normal full name that just the first letter of firstName & lastName is Capitalized (example : Harry Potter)
      */
-    public String normalizingName(String firstName, String lastName){
-        return null;
+    public String normalizingName(String firstName, String lastName)
+
+    {
+
+        char[] F = firstName.toCharArray();
+        char[] L = lastName.toCharArray();
+        if (F[0]>96 && F[0]<123) 
+        {
+            F[0] = (char) (F[0] - 32);
+        }
+        if (L[0]>96 && L[0]<123) 
+        {
+            L[0] = (char) (L[0] - 32);
+        }
+        for (int i = 1; i < firstName.length(); i++)
+        {
+            if (F[i]>64 && F[i]<91)
+            {
+                F[i] = (char) (F[i] + 32);
+            }
+        }
+        for (int i = 1; i < lastName.length(); i++)
+        {
+            if (L[i]>64 && L[i]<91)
+            {
+                L[i] = (char) (L[i] + 32);
+            }
+        }
+        String FF = new String(F);
+        String LL = new String(L);
+        FF = FF + " "+LL;
+        return FF;
+
     }
 
     /**
@@ -28,8 +102,26 @@ public class Advanced {
      * @param word This input could have Consecutive repeated letters or not
      * @return if word contains Consecutive repeated letters, one of the repeated letters should be omitted
      */
-    public String doubleChar(String word) {
-        return null;
+    public String doubleChar(String word)
+    {
+        char[] W = word.toCharArray();
+        int j = 1; 
+        for (int i = 0; i < word.length() ; i++) 
+        {
+            while (j < word.length()) 
+            {
+                if (W[i] == W[j]) 
+                {
+                    for (int k = i; k < word.length(); k++) 
+                    {
+                        W[k] = W[(k+1)];
+                    }    
+                }
+                j ++;    
+            }
+            j = i+1;
+        }
+        String newword = new String(W);
+        return newword;
     }
 }
-
--- Base Structure/src/main/java/Warmup.java
+++ Base Structure/src/main/java/Warmup.java
@@ -5,8 +5,49 @@ public class Warmup {
      * In the first function, your inputs are a number and a sentence
      * @return is the number th word of the sentence
      */
-    public String wordFinder(String sentence, int number) {
-        return null;
+    public String wordFinder(String sentence, int number)
+    {
+
+        char[] sen = sentence.toCharArray();
+        char[][] bakhsh = new char[50][50];
+        int j=0;
+        int k=0;
+        for (int i = 0; i < sentence.length() ; i++) 
+        {
+            if (sen[i] == ' ')
+            {
+                j++;
+                k=0;
+            }
+            bakhsh[j][k]=sen[i];
+            k++;
+        }
+        char[] J = new char[100];
+        int z;
+        int i = 0;
+        if ((number-1) > j)
+        {
+            char[] j1 = {'N', 'u', 'm', 'b', 'e', 'r', ' ', '=', ' ', ' ', ' ', 'i', 's', ' ', 'o', 'u', 't', ' ', 'O', 'f', ' ', 'B', 'o', 'u', 'n', 'd', ' '};
+            J = j1;
+            J[9]= (char) number;
+        }
+        else
+        {
+            for ( i = 0; bakhsh[number-1][i] != '\0'; i++)
+            {
+                J[i] = bakhsh[number-1][i];
+            }
+        }
+        z=i;
+        char[] J1 = new char[z];
+        for (int l = 0; l <= z; l++)
+        {
+            J1[l] = J[l];
+        }
+        String dd = new String(J1);
+//        System.out.println(dd);
+//        System.out.println("dvds");
+        return dd;
     }
 
     /**
@@ -15,8 +56,29 @@ public class Warmup {
      * @param searchForEven is a boolean entry
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
      */
-    public int oddEvenCounter(String number, boolean searchForEven) {
-        return -1;
+    public int oddEvenCounter(String number, boolean searchForEven)
+    {
+        char[] num = number.toCharArray();
+        int e=0,O=0;
+        for (int i = 0; i < number.length() ; i++)
+        {
+            if (num[i] == '0' || num[i] == '2' || num[i] == '4' || num[i] == '6'|| num[i] == '8')
+            {
+                e ++;
+            }
+            else
+            {
+                O ++;
+            }
+        }
+        if (searchForEven == true)
+        {
+            return e;
+        }
+        else
+        {
+            return O;
+        }
     }
 
     /**
@@ -24,7 +86,25 @@ public class Warmup {
      * @param wordB --> second word
      * @return The word that is first in alphabet column
      */
-    public String firstWord(String wordA, String wordB) {
-        return null;
+    public String firstWord(String wordA, String wordB)
+    {
+        boolean flag = true;
+        char[] A = wordA.toCharArray();
+        char[] B = wordB.toCharArray();
+        for (int i = 0; i < wordA.length(); i++)
+        {
+            if (A[i] < B[i])
+            {
+                flag = false;
+            }
+        }
+        if (flag == true)
+        {
+            return wordA;
+        }
+        else
+        {
+            return wordB;
+        }
     }
 }
